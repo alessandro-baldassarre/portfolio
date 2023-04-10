55259f383fdd21a2e0e216a9d6037e81ed214de6
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-04-10 23:01:12 +0200

    Update hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
                 <Computers />
             </div>
 
-            <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center" >
+            <div className="absolute xs:bottom-10 bottom-16 w-full flex justify-center items-center" >
                 <a href="#about">
                     <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2">
                         <motion.div animate={{ y: [0, 24, 0], }} transition={{ duration: 2, repeat: Infinity, repeatType: "loop", }} className='w-3 h-3 rounded-full bg-white mb-1' />
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -12,7 +12,7 @@ const Computers = () => {
             <pointLight intensity={1} />
             <spotLight position={[-20, 120, 120]} angle={0.12} penumbra={1} intensity={1.7} castShadow shadow-mapSize={1024} />
             {/* <primitive object={computer.scene} scale={0.75} position={[0, -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]} /> */}
-            <primitive object={computer.scene} scale={(viewport.width < 15) ? 2.2 : (viewport.width / 5.5)} position={[-6.5, -3, -0.9]} rotation={[-0.00, 2, -0.0]} />
+            <primitive object={computer.scene} scale={(viewport.width < 15) ? 2.2 : (viewport.width / 5.5)} position={[-6, -2.6, -0.9]} rotation={[-0.00, 2, -0.0]} />
         </mesh>
     )
 }
