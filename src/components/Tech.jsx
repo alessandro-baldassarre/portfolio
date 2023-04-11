8c7a98e0884bf1e84e56fad552fb650ce19642f6
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-04-11 09:54:14 +0200

    Complete tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import SectionWrapper from '../hoc/SectionWrapper'
+import { technologies } from '../constants'
+import Ball from './canvas/Ball'
 
 const Tech = () => {
     return (
-        <div>Tech</div>
+        <div className="flex flex-row flex-wrap justify-center gap-10">
+            {technologies.map((technologie) => (
+                <div key={technologie.name} className="w-28 h-28">
+                    <Ball icon={technologie.icon} />
+                </div>
+            ))}
+        </div>
     )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "")
